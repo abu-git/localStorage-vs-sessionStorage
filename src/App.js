150607c1d7e5e8f7a91e11692f78d88f44295b24
abu-git <abu.sammie@gmail.com>
2020-07-25 13:23:56 +0200

    localStorage vs sessionStorage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+
+   //set data to local storage
+   setData(){
+
+      let obj = {name: 'Sam Smith', age: 12, email:'samsmith@gmail.com'};
+      //localStorage.setItem('myData', JSON.stringify(obj));
+
+      //use session storage instead
+      sessionStorage.setItem('myData', JSON.stringify(obj));
+
+   }
+   //get data from local storage
+   getData(){
+      let data = sessionStorage.getItem('myData');
+      data = JSON.parse(data);
+      console.log(data.name);
+   }
+
+   render(){
+      return (
+         <div className="App">
+            <button onClick={() => this.setData()}>Set Data</button>
+            <button onClick={() => this.getData()}>Get Data</button>
+         </div>
+      );
+   }
+
 }
 
 export default App;
